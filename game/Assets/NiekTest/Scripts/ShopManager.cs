cf1f62bf622fe0e64107391290acd069b13ac7e6
LusseveldNiek <97088465@st.deltion.nl>
2022-09-09 14:21:24 +0200

    ShopUI

---
+++ game/Assets/NiekTest/Scripts/ShopManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    public static ShopManager instance;
+    public GameObject shopUI;
+    //public Upgrade[] upgrades;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ToggleShop()
+    {
+        shopUI.SetActive(!shopUI.activeSelf);
+    }
+
+
+}

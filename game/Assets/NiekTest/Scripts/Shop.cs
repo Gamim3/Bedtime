f6387f75025ae1eedbe7e652d2939bfcfb863641
LusseveldNiek <97088465@st.deltion.nl>
2022-09-12 10:25:36 +0200

    Shop System

--- game/Assets/NiekTest/Scripts/Shop.cs
+++ game/Assets/NiekTest/Scripts/Shop.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
     public float currency;
+    public Text currencyText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currencyText = GameObject.Find("CurrencyValue").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currencyText.text = currency.ToString();
     }
 
     public void LoadMenu()

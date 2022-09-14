0096c9141e1b088c1bbc81f98ae49ab5a51c2db4
LusseveldNiek <97088465@st.deltion.nl>
2022-09-14 13:56:59 +0200

    Betere Code

--- game/Assets/NiekTest/Scripts/Waypoints.cs
+++ game/Assets/NiekTest/Scripts/Waypoints.cs
@@ -5,15 +5,5 @@ using UnityEngine;
 public class Waypoints : MonoBehaviour
 {
     public Transform[] waypoints;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }

a339f11e29365203bc34ad984a6a2097639a8557
LusseveldNiek <97088465@st.deltion.nl>
2022-09-19 13:05:13 +0200

    Scriptable object

--- game/Assets/NiekTest/Scripts/EnemyStats.cs
+++ game/Assets/NiekTest/Scripts/EnemyStats.cs
@@ -5,38 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New EnemyData", menuName = "Enemy Data", order = 51)]
 public class EnemyStats : ScriptableObject
 {
-    
-    [SerializeField]
-    private string EnemyName;
-    [SerializeField]
-    private float health;
-    [SerializeField]
-    private Sprite icon;
-    [SerializeField]
-    private int value;
-    [SerializeField]
-    private int attackDamage;
-
-    public float Health
-    {
-        get
-        {
-            return health;
-        }
-    }
-    public int Value
-    {
-        get
-        {
-            return value;
-        }
-    }
-
-    public int AttackDamage
-    {
-        get
-        {
-            return attackDamage;
-        }
-    }
+    public int health;
+    public int value;
+    public int attackDamage;
 }

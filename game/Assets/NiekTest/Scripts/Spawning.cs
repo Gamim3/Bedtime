d46e9dbc41303ce2d818425b12879cb991bcac6d
LusseveldNiek <97088465@st.deltion.nl>
2022-09-23 10:51:43 +0200

    betere code

--- game/Assets/NiekTest/Scripts/Spawning.cs
+++ game/Assets/NiekTest/Scripts/Spawning.cs
@@ -7,104 +7,70 @@ using TMPro;
 public class Spawning : MonoBehaviour
 {
     public float spawnTime;
-    public float[] enemy;
-
     public float wave;
-
     public Text waveCounter;
     public List<GameObject> myObjects;
 
     public bool[] spawn;
     public bool wave2check;
+    public float[] counter;
+    private float[] enemyAmount;
+    private float waveMultiplier;
     void Update()
     {
-        enemy[0] = GameObject.Find("EnemyCounter").GetComponent<EnemyCounter>().counter[0];
-        enemy[1] = GameObject.Find("EnemyCounter").GetComponent<EnemyCounter>().counter[1];
-        enemy[2] = GameObject.Find("EnemyCounter").GetComponent<EnemyCounter>().counter[2];
-        enemy[3] = GameObject.Find("EnemyCounter").GetComponent<EnemyCounter>().counter[3];
-
         wave2check = GameObject.Find("Bullets").GetComponent<Bullet>().wave2check;
 
+        if (wave2check == true)
+        {
+            enemyAmount = GameObject.Find("Bullets").GetComponent<Bullet>().enemyAmount;
+            waveMultiplier = GameObject.Find("Bullets").GetComponent<Bullet>().waveMultiplier;
+            counter[0] = enemyAmount[0] * waveMultiplier;
+            counter[1] = enemyAmount[1] * waveMultiplier;
+            counter[2] = enemyAmount[2] * waveMultiplier;
+            counter[3] = enemyAmount[3] * waveMultiplier;
+        }
 
         int randomIndex = Random.Range(0, 4);
         spawnTime += Time.deltaTime;
         if(spawnTime > 1)
         {
-            if (randomIndex == 0 && spawn[0] == true)
+            if (randomIndex == 0 && counter[0] > 0)
             {
                 spawnTime = 0;
                 GameObject instantiatedObject = Instantiate(myObjects[0], transform.position, Quaternion.identity) as GameObject;
+                counter[0] -= 1;
             }
 
-            if (randomIndex == 1 && spawn[1] == true)
+            if (randomIndex == 1 && counter[1] > 0)
             {
                 spawnTime = 0;
                 GameObject instantiatedObject = Instantiate(myObjects[1], transform.position, Quaternion.identity) as GameObject;
+                counter[1] -= 1;
             }
 
-            if (randomIndex == 2 && spawn[2] == true)
+            if (randomIndex == 2 && counter[2] > 0)
             {
                 spawnTime = 0;
                 GameObject instantiatedObject = Instantiate(myObjects[2], transform.position, Quaternion.identity) as GameObject;
+                counter[2] -= 1;
             }
 
-            if (randomIndex == 3 && spawn[3] == true)
+            if (randomIndex == 3 && counter[3] > 0)
             {
                 spawnTime = 0;
                 GameObject instantiatedObject = Instantiate(myObjects[3], transform.position, Quaternion.identity) as GameObject;
+                counter[3] -= 1;
             }    
         }
 
         //for(int i = 20; i > enemy1; i--)
         //{  
         //}
-
-        if (enemy[0] < 1)
-        {
-            spawn[0] = false;
-        }
-
-        else
-        {
-            spawn[0] = true;
-        }
-        
-        if (enemy[1] < 1)
-        {
-            spawn[1] = false;
-        }
-
-        else
-        {
-            spawn[1] = true;
-        }
-
-        if (enemy[2] < 1)
-        {
-            spawn[2] = false;
-        }
-
-        else
-        {
-            spawn[2] = true;
-        }
-
-        if (enemy[3] < 1)
-        {
-            spawn[3] = false;
-            //myObjects.Remove(ScriptableObject.CreateInstance("Enemy 1") as GameObject);
-        }
-
-        else
-        {
-            spawn[3] = true;
-        }
        
         if(wave2check == true)
         {
             wave += 1;
             waveCounter.text = wave.ToString();
-            
         }
     }
 }

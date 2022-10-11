cac77c4864bda114c5bd32666bfd044c248fd63e
yolojoramking <joram.klinkenberg@gmail.com>
2022-10-11 14:39:49 +0200

    script script script script script script

--- game/Assets/menu stuff/menu scripts/SettingsMenu.cs
+++ game/Assets/menu stuff/menu scripts/SettingsMenu.cs
@@ -7,42 +7,38 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    //terug knop
     public void Back()
     {
         SceneManager.LoadScene("main menu");
     }
 
-    public AudioMixer audioMixer;
-
-    public void SetVolume (float volume)
+    //fullscreen knop
+    public void Fullscreen()
     {
-        audioMixer.SetFloat ("volume", volume);
+        Screen.fullScreen = !Screen.fullScreen;
     }
 
-    public void SetFullscreen (bool isFullscreen)
-    {
-        Screen.fullScreen = isFullscreen;
-    }
-
-    Resolution[] resolutions;
+    //resolutie balk
 
     public TMPro.TMP_Dropdown resolutionDropdown;
 
-    private void Start()
+    Resolution[] resolutions;
+
+    void Start()
     {
         resolutions = Screen.resolutions;
 
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
-
         int currentResolutionIndex = 0;
+
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
             {
                 currentResolutionIndex = i;
             }
@@ -52,4 +48,12 @@ public class SettingsMenu : MonoBehaviour
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
     }
+
+    public void SetResolution(int resolutionIndex) 
+    { 
+        Resolution resolution = resolutions[resolutionIndex]; 
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    //geluid slider
 }

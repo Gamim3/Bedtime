72be5ea2d0f5a2e5a0cafeee7d371833ef11a127
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2022-10-27 13:55:05 +0200

    more setup

--- game/Assets/Scripts/Towers/TrainWaypoints.cs
+++ game/Assets/Scripts/Towers/TrainWaypoints.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrainWaypoints : MonoBehaviour
+public class TrainWaypoints : TowerBase
 {
     public GameObject trainTower;
 
